bcc1ff4a68a7a2483696bf4684284ce503424d12
chera <hiiamdiv@yandex.ru>
2024-08-28 23:29:06 +0300

    Created base classes for Student microservice

---
+++ src/main/java/com/cherrrnikov/student/entity/Student.java
@@ -0,0 +1,22 @@
+package com.cherrrnikov.student.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer schoolId;
+}
